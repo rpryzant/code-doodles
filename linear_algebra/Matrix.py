67db5e71b4915e4e2e97e372dcc7e882e0206c8d
Reid Pryzant <rpryzant@quantcast.com>
2016-07-13 18:31:35 -0700

    added matrix.py

---
+++ linear_algebra/Matrix.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+"""
+This file gives a whole bunch of matrix/numpy array utility functions
+
+"""
+def insert_col(m, c):
+    if m is None:
+        return c
+    else:
+        return np.c_[m, c]
+
+
+def insert_row(m, r):
+    if m is None:
+        return r
+    else:
+        return np.r_[m, [r]]
+
+
+def first_nonzero_in_row(r):
+    return next((i for i, x in enumerate(r) if x != 0), None)
+
+def first_zero_in_row(r):
+    return next((i for i, x in enumerate(r) if x == 0), None)
+    
