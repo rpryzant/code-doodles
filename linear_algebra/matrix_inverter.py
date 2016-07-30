67e942fd6bc3d238f15f2f3e429a6a7dac6123b7
rapigan <rapigan@gmail.com>
2016-07-30 10:31:31 -0700

    finished 2 sided inverse

--- linear_algebra/matrix_inverter.py
+++ linear_algebra/matrix_inverter.py
@@ -5,7 +5,26 @@
 
 from math import pow
 
-def submatrix(A, i, j):
+def blank_copy(A):
+    return [row[:] for row in A]
+
+def swap(A, i, j):
+    tmp = A[i][j]
+    A[i][j] = A[j][i]
+    A[j][i] = tmp
+
+def pp(A):
+    """ printpretty """
+    for row in A:
+        print '  '.join(str(x) for x in row)
+
+def multiply_by_scalar(A, c):
+    for i, row in enumerate(A):
+        for j, col in enumerate(A[i]):
+            A[i][j] *= c
+
+
+def minor(A, i, j):
     """
     Returns a new matrix M which is A minus row i & col j
     """
@@ -14,6 +33,36 @@ def submatrix(A, i, j):
         del row[j]
     return M
     
+
+def cofactor(A):
+    """
+    Returns the cofactor matrix of A
+    Each element Cij is the determinant of A's i,jth minor multiplied by -1^{i+j}
+    """
+    C = blank_copy(A)
+    for i, row in enumerate(C):
+        for j, col in enumerate(C[i]):
+            M = minor(A, i, j)
+            detM = determinant(M)            
+            C[i][j] = pow(-1, i+j) * detM
+    return C
+
+def transpose(A):
+    n = len(A[0])
+    for i, row in enumerate(A):
+        for j in range(i, n):
+            swap(A, i, j)
+
+
+def adjoint(A):
+    """
+    Returns the adjoint matrix of A 
+    This is the transpose of A's cofactor matrix
+    """
+    C = cofactor(A)
+    transpose(C)
+    return C
+
 
 def determinant(A, n = None):
     """
@@ -35,14 +84,27 @@ def determinant(A, n = None):
         det = 0
         for j in range(n):
             aij = A[0][j]
-            C = submatrix(A, 0, j)
+            B = minor(A, 0, j)
             # math.pow instead of ** because -1**0 = -1
-            det += pow(-1, 0 + j) * determinant(C) * aij   
+            det += pow(-1, 0 + j) * determinant(B) * aij   
     return det
 
 
-
+def invert(A):
+    """
+    Returns a new matrix that is the two-sided inverse of A
+    A A^-1 = I = A^-1 A
+    """
+    B = blank_copy(A)
+    det = determinant(B)    
+    B = adjoint(B)
+    multiply_by_scalar(B, 1/det)
+    return B
 
 
 test = [[1,2,0],[-1,1,1],[1,2,3]]
+print pp(test)
 print determinant(test)
+
+print pp(cofactor(test))
+print pp(invert(test))
