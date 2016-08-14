a40ddb716a075f9cfdb601226dac3ebad8ced206
rapigan <rapigan@gmail.com>
2016-08-14 13:39:37 -0700

    longest valid done

---
+++ interview_problems/longest_valid_parens/valid.py
@@ -0,0 +1,24 @@
+
+
+def longest_valid(s):
+    l = 0
+    mx = 0
+    stack = []
+    for c in s:
+        if c == '(':
+            stack.append("*")
+            l += 1
+        else:
+            if len(stack) > 0:
+                stack.pop()
+                l += 1
+            else:
+                l = 0
+            if l > mx:
+                mx = l
+
+    return mx
+        
+print longest_valid("(")
+print longest_valid(")()")
+print longest_valid(")()())")
