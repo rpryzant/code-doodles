e40973721253c7f86ed967ef9ca705e00d28d5fa
rapigan <rapigan@gmail.com>
2016-08-18 09:10:43 -0700

    8.4

---
+++ interview_problems/CRACKING/8.4.py
@@ -0,0 +1,12 @@
+
+
+
+def __subsets(S, B):
+    if not S:
+        return B
+    return __subsets(S[1:], [x[:] + [S[0]] for x in B]) + __subsets(S[1:], [x[:] for x in B])
+
+def subsets(S):
+    return __subsets(S, [[]])
+
+print subsets([1,2,3])
