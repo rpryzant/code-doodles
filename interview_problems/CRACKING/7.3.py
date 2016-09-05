272ab85bb64ebdb544987c193ff303e8db64d524
rapigan <rapigan@gmail.com>
2016-09-04 20:46:35 -0700

    7.3

---
+++ interview_problems/CRACKING/7.3.py
@@ -0,0 +1,50 @@
+# jukebox
+
+
+
+class Jukebox:
+    self.CDplayer
+    self.user
+    self.cdColleciton []
+    
+    constructor
+
+    def getCurrentSong()
+    
+    def setUser(user)
+
+class CDPlayer:
+    self.playlist
+    self.cd
+
+    constructors
+
+    def playsong(song)
+    
+    def getplaylist()
+    def setPlaylist(playlist)
+
+    def getCD()
+    def setCD(cd)
+
+
+class playlist:
+    self.song
+    self.queue
+
+    basically a wrapper class for a queue
+
+    def getnextSongUp()
+
+    def queueSong()
+
+class CD:
+    data for id, artist, songs, etc 
+
+class Song:
+    data for id, CD, title, length, etc
+
+class User:
+    self.name
+    
+    get name, set name, get id, set id, constructor, add user, etc
