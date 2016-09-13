f5b534238270a6f980759d63c84270bf11886d65
rapigan <rapigan@gmail.com>
2016-09-13 09:17:22 -0700

    4.11

--- interview_problems/CRACKING/4.11.py
+++ interview_problems/CRACKING/4.11.py
@@ -1,13 +1,26 @@
+"""
+I like my solution to this one!
 
+let the size of the subtree rooted at node i be k
+    - pick node i w prob 1/k
+    - branch left w/prob k_l / k
+    - branch right w/prob k_r / k
+
+note that if not left or right than k_l or k_r will be 0
+
+
+"""
 
 
 class BTNode:
     def __init__(self, x):
         self.data = x
+        self.size = 1
         self.left = None
         self.right = None
         
     def insert(self, x):
+        self.size += 1
         if x < self.data:
             if not self.left:
                 self.left = BTNode(x)
@@ -49,3 +62,12 @@ class BTNode:
         elif self.right:
             return self.right.find(x)
         return None
+
+
+    def pick_random(self):
+        if random() < 1.0/self.size:
+            return self
+        elif self.left and random < self.left.size / self.size:
+            return self.left.pick_random()
+        else:
+            return self.right.pick_random()
