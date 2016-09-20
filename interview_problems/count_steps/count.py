7539c803c39d61b70d528186f565e80fde4ef269
rapigan <rapigan@gmail.com>
2016-09-20 09:05:28 -0700

    count

---
+++ interview_problems/count_steps/count.py
@@ -0,0 +1,20 @@
+
+
+
+def count(n):
+    return countDP(n, [-1 for _ in range(n+1)])
+
+
+def countDP(n, m):
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    elif m[n] > -1:
+        return m[n]
+    m[n] = countDP(n-1, m) + countDP(n-2, m)
+    return m[n]
+
+
+print count(15)
+print count(3)
