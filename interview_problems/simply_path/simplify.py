ac16f31bbe47f61593db85be0276487526fdef5a
rapigan <rapigan@gmail.com>
2016-09-20 09:39:42 -0700

    simplify path

---
+++ interview_problems/simply_path/simplify.py
@@ -0,0 +1,24 @@
+
+
+import re
+
+
+TOKEN_PATTERN = "\/(\w+|\.\.)"
+
+
+def simplify(path):
+    tokens = re.findall(TOKEN_PATTERN, path)
+    s = []
+    for t in tokens:
+        if t == ".." and len(s) > 0:
+            s.pop()
+        elif t != "..":
+            s.append(t)
+    return "/" + '/'.join(x for x in s)
+
+
+print simplify("/home/")
+print simplify("/a/./b/../../c/")
+print simplify("//")
+print simplify("..")
+print simplify("/../")
