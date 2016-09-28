aceb732ded77f2a02040d98155a73dc5ad53d6b0
rapigan <rapigan@gmail.com>
2016-09-28 08:08:11 -0700

    kth smallest

---
+++ interview_problems/k_th_smalest_bst/kthSmallest.py
@@ -0,0 +1,25 @@
+
+
+def size(n, d):
+    if n.left:
+        size(n.left, d)
+    if n.right:
+        size(n.right, d)
+    d[id(n)] = d[id(n.left)] + d[id(n.right)] + 1
+
+
+def kth_from_last(root, k):
+    def _kfl(n, skipped):
+        index = d[id(n.left)] + skipped + 1
+        if index == k:
+            return n
+        elif index < k:
+            return _kfl(n.right, index)
+        else:
+            return _kfl(n.left, skipped)
+    d = {}
+    size(root, d)
+    return _kfl(root, 0)
+
+
+
