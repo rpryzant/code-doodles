cf9dac5f9a832b7ad0abd18a502f78b23c10ea5a
rapigan <rapigan@gmail.com>
2016-09-29 06:34:48 -0700

    2.3 - delete internal node

---
+++ interview_problems/CRACKING/2.3.py
@@ -0,0 +1,11 @@
+
+
+def delete(n):
+    if n is None or n.next is None:
+        return False
+    n.data = n.next.data
+    tmp = n.next
+    n.next = n.next.next
+    tmp.next = None
+    return True
+               
