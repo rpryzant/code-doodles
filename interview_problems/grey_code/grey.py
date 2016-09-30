090808c690945608941deb12d826aa35b9e9327c
rapigan <rapigan@gmail.com>
2016-09-30 07:57:30 -0700

    grey code converter

---
+++ interview_problems/grey_code/grey.py
@@ -0,0 +1,32 @@
+
+
+
+def grey_code_seq(n):
+    out = []
+    for i in range(n+1):
+
+        out.append(b2s(n2g(i)))
+    return out
+
+
+def b2s(n):
+    s = ""
+    while n > 0:
+        s = ('1' if n & 1 == 1 else '0') + s
+        n >>=1
+    return s
+
+def n2g(n):
+    print n, b2s(n), b2s(msb(n))
+    return (n ^ (n >> 1))
+
+
+def msb(n):
+    mask = 1 << 31
+    while n & mask == 0 and mask > 0:
+        mask >>= 1
+    return mask
+
+
+
+print grey_code_seq(7)
