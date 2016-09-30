46dc8c4dad59f3e0280d344ef5bf0adb5967a1fe
rapigan <rapigan@gmail.com>
2016-09-30 07:02:57 -0700

    4.6 - in order sucessor of bst

---
+++ interview_problems/CRACKING/4.6.py
@@ -0,0 +1,16 @@
+
+
+
+def get_successor(n):
+    if n is None:
+        return None
+    if n.right is not None:
+        n = n.right
+        while n.left is not None:
+            n = n.left
+        return n
+    else:
+        while id(n) != id(n.parent.left) and n.parent is not None:
+            n = n.parent
+        return n.parent
+            
