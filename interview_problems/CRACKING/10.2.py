aa97c148271531e0e4dfeb418757b66c78477533
rapigan <rapigan@gmail.com>
2016-10-05 08:13:29 -0700

    10.2 - group anagrams

---
+++ interview_problems/CRACKING/10.2.py
@@ -0,0 +1,11 @@
+
+
+
+def group_anagrams(A):
+    Ap = sorted([(sorted(w), i) for i, w in enumerate(A)], key = lambda x: x[0])
+    for i, (char_array, index) in enumerate(Ap):
+        Ap[i] = A[index]
+    return Ap
+
+
+print group_anagrams(['hello', 'good','doog', 'olelh'])
