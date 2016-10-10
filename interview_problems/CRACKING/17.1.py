f0ca6b21e10759b3b7282a7c3e1589bf4bd31a91
rapigan <rapigan@gmail.com>
2016-10-10 07:19:20 -0700

    17.1 - bitwise add

---
+++ interview_problems/CRACKING/17.1.py
@@ -0,0 +1,15 @@
+
+
+
+def add(a, b):
+    result = a ^ b
+    carry = (a & b) << 1
+    while carry > 0:
+        tmp = result ^ carry
+        carry = (result & carry) << 1
+        result = tmp
+    return result
+
+print add(1, 1)
+print add(5, 6)
+print add(5, 11)
