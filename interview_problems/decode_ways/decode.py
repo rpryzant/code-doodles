e9beb3cffad01cd00dcefb0cbc31822e7160c0ba
rapigan <rapigan@gmail.com>
2016-10-20 11:27:26 -0700

    decode update

--- interview_problems/decode_ways/decode.py
+++ interview_problems/decode_ways/decode.py
@@ -30,3 +30,42 @@ print num_ways('12')
 print num_ways('9999')
 
 print num_ways('1222')
+
+
+
+
+
+
+
+
+
+def can_decode(pair):
+    return 10 <= int(pair) and int(pair) <= 26
+
+def num_ways(M):
+    cache = {}
+    def nw(i):
+        if cache.get(i, False):
+            return cache[i]
+        if i == 0 and M[0] == '0' or i < 0:
+            cache[i] = 0
+        if i == 0:
+            cache[i] = 1
+        else:
+            cache[i] = nw(i-1) + (1 if M[i-1] != '0' and can_decode(M[i-1:i+1]) else 0)
+
+        return cache[i]
+    
+    return nw(len(M) - 1)
+
+
+
+
+
+print num_ways('')
+print num_ways('123')
+print num_ways('12')
+print num_ways('99999')
+print num_ways('12022')
+print num_ways('0')
+print num_ways('100')
