6b7a10a997d590b6e9ba818d7b838baab0dcb4eb
rapigan <rapigan@gmail.com>
2016-11-06 07:57:40 -0800

    palindrome partitioning

---
+++ interview_problems/palindrome_partitioning/partition.py
@@ -0,0 +1,26 @@
+
+
+
+def isPalin(s):
+    return s == s[::-1]
+
+def palinPartition(s):
+    if not s:
+        return []
+    out = []
+
+    def dfs(i, sofar):
+        if i == len(s):
+            out.append(sofar[:])
+            return
+        for j in range(i+1, len(s)+1):
+            chunk = s[i:j]
+            if isPalin(chunk):
+                sofar.append(chunk)
+                dfs(j, sofar)
+                sofar.pop()
+
+    dfs(0, [])
+    return out
+
+print palinPartition('aab')
