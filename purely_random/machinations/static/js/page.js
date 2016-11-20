e9791f5d400ab77785b6b250beb83b74e9fc03a6
rapigan <rapigan@gmail.com>
2016-11-20 15:13:55 -0800

    website up

---
+++ purely_random/machinations/static/js/page.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+        var picNum = Math.floor(Math.random() * 14);
+
+
+
+        $('#outer-frame').css("background", "url(static/img/bg/bg" + picNum + ".png)");
+});
+    
