1900b59781f10e41684d24fab0a26963404e17e6
rapigan <rapigan@gmail.com>
2017-08-21 20:29:34 -0700

    comments

--- purely_random/imdb_stuff/movie_genre_extractor.py
+++ purely_random/imdb_stuff/movie_genre_extractor.py
@@ -10,13 +10,17 @@ from tqdm import tqdm
 
 
 def gen_imdb_genres(query):
+    # query imdb 
     url = 'http://www.imdb.com/find?s=all&q=%s' % urllib.quote(query)
     r = urllib.urlopen(url).read()
     s = BeautifulSoup(r)
+
+    # select top hit
     table = s.findAll('table', {'class': 'findList'})[0]
     top_hit = table.find_all('tr')[0]
     url = top_hit.findAll('a')[-1]['href']
 
+    # get genre info
     url = 'http://www.imdb.com/' + url
     r = urllib.urlopen(url).read()
     s = BeautifulSoup(r)
