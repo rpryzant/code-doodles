4b60d38840e61b236836ae9654dbf6c4861bd9e9
Reid Pryzant <rapigan@gmail.com>
2017-08-25 08:21:53 -0700

    Update nmt_tools.sh

--- msc_nlp/nmt_tools.sh
+++ msc_nlp/nmt_tools.sh
@@ -1,1 +1,30 @@
 
+
+
+# run inference with tensorflow.nmt
+python -m nmt.nmt \
+    --src=ja --tgt=en \
+    --ckpt=4_1/output/best_bleu/translate.ckpt-90000 \
+    --hparams_path=params.json \
+    --out_dir=INFERENCE_OUTPUT \
+    --vocab_prefix=4_1/vocab \
+    --inference_input_file=4_1/test.ja \
+    --inference_output_file=INFERENCE_OUTPUT/trans \
+    --inference_ref_file=4_1/test.en
+
+
+# run training with tensorflow.nmt
+python -m nmt.nmt \
+    --src=ja --tgt=en \
+    --hparams_path=params.json \
+    --out_dir=jesc/output/ \
+    --vocab_prefix=jesc/vocab \
+    --train_prefix=jesc/train \
+    --dev_prefix=jesc/val \
+    --test_prefix=jesc/test
+
+
+
+
+
+
