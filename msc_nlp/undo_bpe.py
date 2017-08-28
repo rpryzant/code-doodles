f5b6954279c3f355229f1fd02e9b673af5eaecf5
Reid Pryzant <rapigan@gmail.com>
2017-08-27 20:01:56 -0700

    Update undo_bpe.py

--- msc_nlp/undo_bpe.py
+++ msc_nlp/undo_bpe.py
@@ -1,14 +1,8 @@
 # -*- coding: utf-8 -*-                                                                                                                                                  
 # undoes a bpe word encoding
 # python undo_bpe.py input > output
-
-
 import sys
-
 
 for l in open(sys.argv[1]):
     l = l.strip().split()
     print ''.join(l).replace('_', ' ').replace('▁', ' ').strip()
-
-
-
