afa81c9b76428d3c52230eebbcb187ee4d09cc5d
Reid Pryzant <rpryzant@Reids-MacBook-Pro.local>
2017-09-27 14:20:56 -0700

    search 2

---
+++ interview_problems/search_rotated_array/search2.py
@@ -0,0 +1,38 @@
+
+# this works!!
+
+def s(a, t, l, h):
+    if l > h:
+        return -1
+
+    m = (l+h) / 2
+    if a[m] == t:
+        return m
+
+    if a[l] > a[h]:
+        if a[m] > a[h]:  # pivot to right of mid
+            if t > a[m] or t < a[h]:
+                return s(a, t, m+1, h)
+            else:
+                return s(a, t, l, m-1)
+        else:            # piv to lft
+            if t > a[m] and t <= a[h]:
+                return s(a, t, m+1, h)
+            else:
+                return s(a, t, l, m-1)
+                
+    else:
+        if a[m] < t:
+            return s(a, t, m+1, h)
+        else:
+            return s(a, t, l, m-1)
+
+test = [4,5,6,7,0,1,2]
+print s(test, 5, 0, len(test) - 1)
+print s(test, 1, 0, len(test) - 1)
+print s(test, 10, 0, len(test) - 1)
+            
+
+# THIS BREAKS IT!!!
+test2 = [8,19, 1,2,3,4,5]
+print s(test2, 19, 0, len(test) - 1)
