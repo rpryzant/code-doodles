9d2f620cfa6a30bf4cb2ccc552addcc5eb397c4f
rpryzant <rapigan@gmail.com>
2017-10-10 18:59:07 -0700

    convenience classes

---
+++ interview_problems/CONVENIENCE_CLASSES.py
@@ -0,0 +1,36 @@
+
+# binary tree
+class Node:
+    def __init__(self, d):
+        self.data = d
+        self.left = None
+        self.right = None
+
+    def printpretty(self):
+        self._printpretty(self, "")
+
+    def _printpretty(self, node, s):
+        if node:
+            self._printpretty(node.right, s + "\t")
+            print s + str(node.data)
+            self._printpretty(node.left, s + "\t")        
+
+# linked list
+class Node:
+    def __init__(self, data):
+        self.next = None
+        self.data = data
+
+    def append(self, new):
+        while self.next is not None:
+            self = self.next
+        self.next = new
+
+    def __str__(self):
+        s = ''
+        while self is not None:
+            s += '%s => ' % self.data
+            self = self.next
+        return s
+
+
