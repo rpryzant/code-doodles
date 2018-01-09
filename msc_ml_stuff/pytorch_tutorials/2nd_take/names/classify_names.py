19f725822980182f0766b8e3c68b12f297364590
rpryzant <rapigan@gmail.com>
2018-01-09 13:50:31 -0800

    name classification

--- msc_ml_stuff/pytorch_tutorials/2nd_take/names/classify_names.py
+++ msc_ml_stuff/pytorch_tutorials/2nd_take/names/classify_names.py
@@ -55,4 +55,81 @@ def lineToTensor(line):
     return tensor
 
 # make the model
-import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.optim as optim
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return Variable(torch.zeros(1, self.hidden_size))
+
+
+n_categories = len(all_categories)
+n_hidden = 128
+
+def categoryFromOutput(output):
+    top_val, top_i = output.data.topk(1)
+    category_i = top_i[0][0]
+    return all_categories[category_i], category_i
+
+import random
+
+
+def randomChoice(l):
+    return l[random.randint(0, len(l) - 1)]
+
+def randomTrainingExample():
+    category = randomChoice(all_categories)
+    line = randomChoice(category_lines[category])
+    category_tensor = Variable(torch.LongTensor([all_categories.index(category)]))
+    line_tensor = Variable(lineToTensor(line))
+    return category, line, category_tensor, line_tensor
+
+# train
+
+model = RNN(n_letters, n_hidden, n_categories)
+
+criterion = nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.005)
+
+def train(category_tensor, line_tensor):
+    hidden = model.initHidden()
+    model.zero_grad()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = model(line_tensor[i], hidden)
+
+    loss = criterion(output, category_tensor)
+    loss.backward()
+    optimizer.step()
+    return loss.data[0]
+
+losses = []
+for itr in range(1, 100000):
+    if itr % 100 == 0:
+        print itr
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+
+    l = train(category_tensor, line_tensor)
+    losses.append(l)
+
+l = len(losses)
+print sum(losses[:500])
+print sum(losses[-500:])
+
