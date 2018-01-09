ab18fcff2e28fa6c449b8d8dedd103bf5226d5d0
rpryzant <rapigan@gmail.com>
2018-01-09 09:17:39 -0800

    name classifier start

---
+++ msc_ml_stuff/pytorch_tutorials/2nd_take/names/classify_names.py
@@ -0,0 +1,58 @@
+from __future__ import unicode_literals, division
+from io import open
+import glob
+
+# set up category lines:: {language: [names]}
+
+def findFiles(path): return glob.glob(path)
+
+import unicodedata
+import string
+
+all_letters = string.ascii_letters + " .,;'"
+n_letters = len(all_letters)
+
+# Turn a Unicode string to plain ASCII, thanks to http://stackoverflow.com/a/518232/2809427
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+        and c in all_letters
+    )
+
+category_lines = {}
+all_categories = []
+
+# Read a file and split into lines
+def readLines(filename):
+    lines = open(filename, encoding='utf-8').read().strip().split('\n')
+    return [unicodeToAscii(line) for line in lines]
+
+for filename in findFiles('data/names/*.txt'):
+    category = filename.split('/')[-1].split('.')[0]
+    all_categories.append(category)
+    lines = readLines(filename)
+    category_lines[category] = lines
+
+
+# turn names into a list of 1-hots, one per letter
+#  so each name is [word length, 1, num letters]
+import torch
+
+def letterToIndex(letter):
+    return all_letters.find(letter)
+
+def letterToTensor(letter):
+    tensor = torch.zeros(1, n_letters)
+    tensor[0][letterToIndex(letter)] = 1
+    return tensor
+
+# "line" == word/name, really
+def lineToTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for li, letter in enumerate(line):
+        tensor[li][0][letterToIndex(letter)] = 1
+    return tensor
+
+# make the model
+import torch
