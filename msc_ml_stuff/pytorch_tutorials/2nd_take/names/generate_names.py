7009813173b3f77da5afa62c7ce5ca00848c95c3
rpryzant <rapigan@gmail.com>
2018-01-09 14:26:44 -0800

    generate names done

---
+++ msc_ml_stuff/pytorch_tutorials/2nd_take/names/generate_names.py
@@ -0,0 +1,144 @@
+from __future__ import unicode_literals, division
+from io import open
+import glob
+import unicodedata
+import string
+
+######## READ DATA
+
+all_letters = string.ascii_letters + " .,;'-"
+n_letters = len(all_letters) + 1 # Plus EOS marker
+
+def findFiles(path): return glob.glob(path)
+
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+        and c in all_letters
+    )
+
+def readLines(filename):
+    lines = open(filename, encoding='utf-8').read().strip().split('\n')
+    return [unicodeToAscii(line) for line in lines]
+
+# Build the category_lines dictionary, a list of lines per category
+category_lines = {}
+all_categories = []
+for filename in findFiles('data/names/*.txt'):
+    category = filename.split('/')[-1].split('.')[0]
+    all_categories.append(category)
+    lines = readLines(filename)
+    category_lines[category] = lines
+
+n_categories = len(all_categories)
+
+
+########## MODEL DEFINITION
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.optim as optim
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(n_categories + input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(n_categories + input_size + hidden_size, output_size)
+        self.o2o = nn.Linear(hidden_size + output_size, output_size)
+
+        self.dropout = nn.Dropout(0.1)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, category, input, hidden):
+        input_combined = torch.cat((category, input, hidden), 1)
+        hidden = self.i2h(input_combined)
+        output = self.i2o(input_combined)
+        output_combined = torch.cat((hidden, output), 1)
+        output = self.o2o(output_combined)
+        output = self.dropout(output)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return Variable(torch.zeros(1, self.hidden_size))
+
+
+
+####### TRAINING
+import random
+
+# Random item from a list
+def randomChoice(l):
+    return l[random.randint(0, len(l) - 1)]
+
+# Get a random category and random line from that category
+def randomTrainingPair():
+    category = randomChoice(all_categories)
+    line = randomChoice(category_lines[category])
+    return category, line
+
+
+# One-hot vector for category
+def categoryTensor(category):
+    li = all_categories.index(category)
+    tensor = torch.zeros(1, n_categories)
+    tensor[0][li] = 1
+    return tensor
+
+# One-hot matrix of first to last letters (not including EOS) for input
+def inputTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for li in range(len(line)):
+        letter = line[li]
+        tensor[li][0][all_letters.find(letter)] = 1
+    return tensor
+
+# LongTensor of second letter to end (EOS) for target
+def targetTensor(line):
+    letter_indexes = [all_letters.find(line[li]) for li in range(1, len(line))]
+    letter_indexes.append(n_letters - 1) # EOS
+    return torch.LongTensor(letter_indexes)
+
+# Make category, input, and target tensors from a random category, line pair
+def randomTrainingExample():
+    category, line = randomTrainingPair()
+    category_tensor = Variable(categoryTensor(category))
+    input_line_tensor = Variable(inputTensor(line))
+    target_line_tensor = Variable(targetTensor(line))
+    #                         one hot            indices
+    #      [B, C]            [W, B, L]            [W,]
+    return category_tensor, input_line_tensor, target_line_tensor
+
+
+criterion = nn.NLLLoss()
+rnn = RNN(n_letters, 128, n_letters)
+optimizer = optim.SGD(rnn.parameters(), lr=0.0005)
+
+
+def train(category_tensor, input_line_tensor, target_line_tensor):
+    hidden = rnn.initHidden()
+    rnn.zero_grad()
+
+    loss = 0
+
+    for i in range(input_line_tensor.size()[0]):
+        output, hidden = rnn(category_tensor, input_line_tensor[i], hidden)
+        loss += criterion(output, target_line_tensor[i])
+
+    loss.backward()
+    optimizer.step()
+    
+    return loss.data[0] / input_line_tensor.size()[0]
+
+losses = []
+for itr in range(100000):
+    l = train(*randomTrainingExample())
+    losses.append(l)
+
+l = len(losses)
+print sum(losses[:500])
+print sum(losses[-500:])
