222c3fc1e331bebe24aca791550219349370c7ed
rpryzant <rapigan@gmail.com>
2018-03-04 14:34:03 -0800

    yup

--- msc_nlp/rational_speech_acts/RSA.py
+++ msc_nlp/rational_speech_acts/RSA.py
@@ -24,14 +24,14 @@ class RSA:
         return rownorm(self.lexicon * self.prior)
     
     def speaker(self):
-#        lit = self.literal_listener().T
-        lit = pd.DataFrame([[0.0, 1.0], [0.3, 0.7]], index=['h', 'g'], columns=['r1', 'r2']).T
+        lit = self.literal_listener().T
+#        lit = pd.DataFrame([[0.0, 1.0], [0.3, 0.7]], index=['h', 'g'], columns=['r1', 'r2']).T
         utilities = self.alpha * safelog(lit) - self.costs        
         return rownorm(np.exp(utilities))
         
     def listener(self):
-#        spk = self.speaker().T
-        spk = pd.DataFrame([[1.0, 0.0], [0.25, 0.75], [0, 1]], index=['r1', 'r2', 'r3'], columns=['h', 'g']).T
+        spk = self.speaker().T
+#        spk = pd.DataFrame([[1.0, 0.0], [0.25, 0.75], [0, 1]], index=['r1', 'r2', 'r3'], columns=['h', 'g']).T
         return rownorm(spk * self.prior)    
     
 def rownorm(mat):
@@ -45,9 +45,18 @@ def safelog(vals):
 
 
 
-msgs = ['hat', 'glasses']
-states = ['r1', 'r2']
-
+msgs = ['riot', 'violent', 'energetic']
+states = ['pve', 'pv~e', 'pe~v']
+lex = pd.DataFrame([[1., 0., 0.], [1., 1., 0.], [1., 0., 1.]], index=msgs, columns=states)
+mod1 = RSA(
+    lexicon=lex,
+    prior=[0.95, 0.04, 0.01],
+    costs=[0., 0., -5.],
+    alpha=1)
+print mod1.literal_listener()
+print mod1.speaker()
+#print mod1.listener()
+quit()
 
 # q1 
 lex = pd.DataFrame([[1.0, 1.0], [1.0, 0.0]], index=msgs, columns=states)
